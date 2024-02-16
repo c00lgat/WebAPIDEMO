c596007d3dfc5df75bb62654c560c508116d201d
c00lgat <a28_a3@yahoo.com>
2024-02-16 13:51:19 +0200

    Add project files.

---
+++ WebAPIDEMO/Controllers/ProteinsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+
+namespace WebAPIDEMO.Controllers
+{
+    [ApiController]
+    public class ProteinsController:ControllerBase
+    {
+        [HttpGet]
+        public string GetProteins()
+        {
+            return "Reading all the Shirts";
+        }
+        [HttpGet]
+        public string GetProteinById(int id)
+        {
+            return $"Reading Protein with ID: {id}";
+        }
+
+        [HttpPost]
+        public string CreateProteins()
+        {
+            return $"Creating a protein";
+        }
+
+        [HttpPut]
+        public string UpdateProtein(int id)
+        {
+            return $"Reading protein: {id}";
+        }
+        [HttpDelete]
+        public string DeleteProtein(int id)
+        {
+            return $"Deleting protein with ID: {id}";
+        }
+
+    }
+}
